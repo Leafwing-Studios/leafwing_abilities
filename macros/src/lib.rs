c6f0e82d04854e3c87911dcf44ff412056f4a800
Alice Cecile <alice.i.cecile@gmail.com>
2022-10-17 14:55:35 -0400

    Update macros crate

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,17 +1,17 @@
-//! Derives the [`Actionlike`] trait
+//! Derives the [`Abilitylike`] trait
 //
 //! This derive macro was inspired by the `strum` crate's `EnumIter` macro.
 //! Original source: https://github.com/Peternator7/strum,
 //! Copyright (c) 2019 Peter Glotfelty under the MIT License
 
 extern crate proc_macro;
-mod actionlike;
+mod abilitylike;
 use proc_macro::TokenStream;
 use syn::DeriveInput;
 
-#[proc_macro_derive(Actionlike)]
-pub fn actionlike(input: TokenStream) -> TokenStream {
+#[proc_macro_derive(Abilitylike)]
+pub fn abilitylike(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as DeriveInput);
 
-    crate::actionlike::actionlike_inner(&ast).into()
+    crate::abilitylike::abilitylike_inner(&ast).into()
 }
