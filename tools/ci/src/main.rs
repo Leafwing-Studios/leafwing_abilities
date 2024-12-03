8acd12340b207370abc5f89e0c5e8f64b2c04577
Coster <coster.monger@outlook.com>
2024-12-03 16:58:44 +0000

    Update tools/ci to use newer xshell (#69)
    Xshell 1.0 is not compatible with nightly rust, and may not be comatible with upcoming stable rust versions. See https://github.com/matklad/xshell/pull/97

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -1,4 +1,4 @@
-use xshell::cmd;
+use xshell::{cmd, Shell};
 
 fn main() {
     // When run locally, results may differ from actual CI runs triggered by
@@ -6,29 +6,37 @@ fn main() {
     // - Official CI runs latest stable
     // - Local runs use whatever the default Rust is locally
 
+    let sh = Shell::new().unwrap();
+
     // See if any code needs to be formatted
-    cmd!("cargo fmt --all -- --check")
+    cmd!(sh, "cargo fmt --all -- --check")
         .run()
         .expect("Please run `cargo fmt --all` to format your code.");
 
     // See if clippy has any complaints.
     // - Type complexity must be ignored because we use huge templates for queries
-    cmd!("cargo clippy --workspace --all-features -- -D warnings -A clippy::type_complexity")
-        .run()
-        .expect("Please fix `cargo clippy` errors with all features enabled.");
+    cmd!(
+        sh,
+        "cargo clippy --workspace --all-features -- -D warnings -A clippy::type_complexity"
+    )
+    .run()
+    .expect("Please fix `cargo clippy` errors with all features enabled.");
 
     // Check for errors with no features enabled
-    cmd!("cargo check --workspace --no-default-features")
+    cmd!(sh, "cargo check --workspace --no-default-features")
         .run()
         .expect("Please fix `cargo check` errors with no features enabled .");
 
     // Check for errors with default features enabled
-    cmd!("cargo check --workspace")
+    cmd!(sh, "cargo check --workspace")
         .run()
         .expect("Please fix `cargo check` errors with default features enabled.");
 
     // Check the examples with clippy
-    cmd!("cargo clippy --examples -- -D warnings -A clippy::type_complexity")
-        .run()
-        .expect("Please fix `cargo clippy` errors for the examples.");
+    cmd!(
+        sh,
+        "cargo clippy --examples -- -D warnings -A clippy::type_complexity"
+    )
+    .run()
+    .expect("Please fix `cargo clippy` errors for the examples.");
 }
